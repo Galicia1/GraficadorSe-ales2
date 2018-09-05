95e65ea17b03b3d459d1a4838487a23fea02b265
Instalacion de Softawres <software@ulsa-noroeste.edu.mx>
2018-09-05 11:41:16 -0700

    Se agregaron indicadores de Amplitud y las escalamos

--- GraficadorSeñales/MainWindow.xaml.cs
+++ GraficadorSeñales/MainWindow.xaml.cs
@@ -68,9 +68,13 @@ namespace GraficadorSeñales
             //Recorrer una  coleccion o arreglo
             foreach (Muestra muestra in señal.Muestras)
             {
-                plnGrafica.Points.Add(new Point(muestra.X * scrContenedor.Width, (muestra.Y * ((scrContenedor.Height / 2.0) - 30) * -1)
+              
+                plnGrafica.Points.Add(new Point(muestra.X * scrContenedor.Width, ((muestra.Y / señal.AmplitudMaxima) * ((scrContenedor.Height / 2.0) - 30) * -1)
                 + (scrContenedor.Height / 2)));
             }
+
+            lblAmplitudMaximaY.Text = señal.AmplitudMaxima.ToString();
+            lblAmplitudMaximaNegativaY.Text = "-" + señal.AmplitudMaxima.ToString();
 
         }
 
